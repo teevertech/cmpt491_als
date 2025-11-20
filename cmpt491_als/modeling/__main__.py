a7486bbfce20090c951b4bb3d38166130ca3cd96
Stefan Cvijovic <164532223+cvijovics@users.noreply.github.com>
2025-11-20 06:34:45 -0700

    Update __main__.py

--- cmpt491_als/modeling/__main__.py
+++ cmpt491_als/modeling/__main__.py
@@ -4,7 +4,7 @@ from cmpt491_als.modeling.evaluate import app as eval_app
 
 app = typer.Typer()
 
-app.add_typer(train_app, name="train")
+app.add_typer(train_app, name="fit")
 app.add_typer(eval_app, name="eval")
 
 if __name__ == "__main__":
